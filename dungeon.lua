df8d2613ea2b7b4bc929bbc85202b087f6354a5e
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-09-22 23:04:08 -0300

    Create dungeon.lua

---
+++ dungeon.lua
@@ -0,0 +1,21 @@
+function genMap()
+
+    local x = 400
+    local y = 31929
+
+    local width = 400
+    local height = 80
+    local angle = 0
+
+    local lastWidth, lastHeight, lastHeight
+
+    for i = 1,10 do
+
+        local lastWidth, lastHeight, lastHeight = width, height, angle
+
+        width = math.random(50, 100)
+        angle = math.random(0, 360)
+
+        
+    end
+end
