b4770fc00292b78b4bcb84d167a9f888671ba06e
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-10-06 22:07:34 -0300

    remove UI when game starts, load a new map and move each player to each spawnpoint

--- dungeon.lua
+++ dungeon.lua
@@ -1,21 +1,39 @@
+local spawnPoints = {
+
+    [1] = {x =  400, y =  200},
+    [2] = {x = 4400, y =  200},
+    [3] = {x = 6800, y =  200},
+    [4] = {x =  400, y = 4000},
+    [5] = {x = 4400, y = 4000},
+    [6] = {x = 6800, y = 4000},
+    [7] = {x =  400, y = 6800},
+    [8] = {x = 4400, y = 6800},
+    [9] = {x = 6800, y = 6800},
+
+}
+
+local function startGame()
+
+    for i = -1, 13 do
+
+        ui.removeTextArea(i, nil)
+    end
+
+    currentGameState = gameStates.dungeon
+
+    tfm.exec.newGame(dungeonMap)
+
+    local index = 0
+
+    for name in next, players do
+
+        index = index + 1
+
+        tfm.exec.movePlayer(name, spawnPoints[index].x, spawnPoints[index].y)
+    end
+end
+
 function genMap()
 
-    local x = 400
-    local y = 31929
-
-    local width = 400
-    local height = 80
-    local angle = 0
-
-    local lastWidth, lastHeight, lastHeight
-
-    for i = 1,10 do
-
-        local lastWidth, lastHeight, lastHeight = width, height, angle
-
-        width = math.random(50, 100)
-        angle = math.random(0, 360)
-
-        
-    end
+    
 end
