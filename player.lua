b2ac5a65b8093c93c0759113e1efa7da8bddd18d
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-10-06 22:06:17 -0300

    minor changes

--- player.lua
+++ player.lua
@@ -1,25 +1,26 @@
-local function Player(name)
+local Player = {}
+Player.__index = Player
 
-    local instance = {
-        
-        index      = 0,
+function Player:new(name)
 
-        health     = 100,
-        maxHealth  = 100,
+    local player = setmetatable(
+        {
+            index = 0,
 
-        speed      = 5,
+            health = 100,
+            maxHealth = 100,
 
-        isDead     = false,
-        isPlaying  = false,
+            speed = 5,
 
-        backpack   = {
-            items.sword
+            isDead = false,
+            isPlaying = false,
+
+            hotbar = {},
+            backpack = {items.sword},
         },
+    Player)
 
-        hotbar     = {}
-    }
-
-    instance.hotbar[1] = instance.backpack[1]
+    player.hotbar[1] = player.backpack[1]
 
     if currentGameState == gameStates.lobby then
 
@@ -27,45 +28,47 @@ local function Player(name)
         showPlayerList(name)
     end
 
-    function instance:showHotbar()
+    return player
+end
 
-        ui.addTextArea(11, self.hotbar[1].name, name, 10, 345, 50, 50, nil, 0xf, 0.5, true)
+function Player:killPlayer()
+
+    tfm.exec.killPlayer(name)
+    self.isDead = true
+end
+
+function Player:setHealth(health)
+
+    self.health = math.min(health, self.maxHealth)
+end
+
+function Player:addHealth(health)
+
+    self.health = math.min(self.health + health, self.maxHealth)
+end
+
+function Player:subHealth(health)
+
+    self.health = self.health - health
+
+    if self.health <= 0 then
+        self:killPlayer()
+    end
+end
+
+function Player:showHotbar()
+
+    ui.addTextArea(11, self.hotbar[1].name, name, 10, 345, 50, 50, nil, 0xf, 0.5, true)
         
-        if self.hotbar[2] then
-            ui.addTextArea(12, self.hotbar[2].name, name, 75, 345, 50, 50, nil, 0xf, 0.5, true)
-        end
-        if self.hotbar[3] then
-            ui.addTextArea(13, self.hotbar[3].name, name, 140, 345, 50, 50, nil, 0xf, 0.5, true)
-        end
+    if self.hotbar[2] then
+        ui.addTextArea(12, self.hotbar[2].name, name, 75, 345, 50, 50, nil, 0xf, 0.5, true)
     end
-
-    function instance:killPlayer()
-
-        tfm.exec.killPlayer(name)
-        self.isDead = true
+        
+    if self.hotbar[3] then
+        ui.addTextArea(13, self.hotbar[3].name, name, 140, 345, 50, 50, nil, 0xf, 0.5, true)
     end
+end
 
-    function instance:setHealth(health)
+function Player:keyboard(key, down, x, y)
 
-        self.health = math.min(health, self.maxHealth)
-    end
-
-    function instance:addHealth(health)
-
-        self.health = math.min(self.health + health, self.maxHealth)
-    end
-
-    function instance:subHealth(health)
-
-        self.health = self.health - health
-        if self.health <= 0 then
-            self:killPlayer()
-        end
-    end
-
-    function instance:keyboard(key, down, x, y)
-
-    end
-
-    return instance
 end
