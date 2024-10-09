9a66b6904dcf11067351590bb99d263d5db90a09
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-10-08 23:35:46 -0300

    add xml class

---
+++ include/xml.lua
@@ -0,0 +1,37 @@
+local Xml = {}
+Xml.__index = Xml
+
+function Xml:new()
+
+    local xml = setmetatable(
+        {
+
+            mapParameters = {},
+            gameElements = {},
+
+            floors = {},
+            shamanObjects = {}
+        },
+    Xml)
+
+    return xml
+end
+
+function Xml:build()
+
+    local mapCode = ""
+
+    return [[
+        <C>
+            <P/>
+            <Z>
+                <S>
+                </S>
+                <D>
+                </D>
+                <O>
+                </O>
+            </Z>
+        </C>
+    ]]
+end
