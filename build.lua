8dfce9459a3f2574cdfe3cb8fb1ef8e5c5cb4b4c
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-10-08 23:35:30 -0300

    add includes

--- build.lua
+++ build.lua
@@ -1,12 +1,21 @@
-function build(files)
+function build(files, includes)
 
     local content = ""
     local build = io.open("builds/build.lua", "w")
     
+    for k, v in next, includes do
+
+        local file = io.open("include/" .. v .. ".lua")
+        content = "-- include " .. v .. ".lua \n" .. content .. file:read("*a") .. "\n\n"
+        file:close()
+    end
+
+    content = content .. "-- end includes\n\n"
+
     for k, v in next, files do
 
         local file = io.open(v .. ".lua")
-        content = content ..  file:read("*a") .. "\n\n"
+        content = content .. file:read("*a") .. "\n\n"
         file:close()
 
     end
@@ -14,6 +23,9 @@ function build(files)
     build:write(content)
 end
 
-build({"init", "items", "lobby", "player", "events"})
+local includes = {"xml"}
+local files = {"init", "items", "lobby", "dungeon", "player", "events"}
+
+build(files, includes)
 
 return
