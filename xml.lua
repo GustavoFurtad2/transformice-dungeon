15ad644b19babdd386fddfcb4f97051bb4a9f4d9
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-10-24 23:48:05 -0300

    add cheeses, holes and decorations support

--- xml.lua
+++ xml.lua
@@ -6,10 +6,16 @@ function Xml:new()
     local xml = setmetatable(
         {
 
-            mapParameters = {},
+            mapParameters = {
+
+                width  = 800,
+                height = 400 
+            },
+
             gameElements = {},
 
             floors = {},
+            decorations = {},
             shamanObjects = {},
 
             holes = {},
@@ -47,6 +53,26 @@ function Xml:addFloor(type, x, y, width, height, friction, restitution)
     })
 end
 
+function Xml:addDecoration(x, y, type, foreground, reverse, reverseRandom)
+
+    table.insert(self.decorations, {
+        
+        type = type,
+        
+        x = x, 
+        y = y,
+    
+        foreground = tostring(foreground == true and 1 or 0),
+        reverse = tostring(reverseRandom or (reverse == true and 1 or 0)),
+    
+    })
+end
+
+function Xml:setMapSize(w, h)
+
+    self.mapParameters.width, self.mapParameters.height = w, h
+end
+
 function Xml:setPlayerSpawn(x, y)
 
     self.playerSpawnX, self.playerSpawnY = x, y
@@ -82,7 +108,7 @@ end
 
 function Xml:build()
 
-    local floors, holes, cheeses = "", "", ""
+    local floors, holes, cheeses, decorations = "", "", "", ""
 
     for _, floor in next, self.floors do
 
@@ -99,8 +125,13 @@ function Xml:build()
         cheeses = cheeses .. string.format([[<F X="%s" Y="%s"/>]], cheese.x, cheese.y)
     end
 
+    for _, decoration in next, self.decorations do
+
+        decorations = decorations .. string.format([[<P X="%s" Y="%s" T="%s" P="%s,%s"/>]], decoration.x, decoration.y, decoration.type, decoration.foreground, decoration.reverse)
+    end
+
     local shamanSpawn = self.shamanSpawnX and string.format([[<DC X="%s" Y="%s"/>]], self.shamanSpawnX, self.shamanSpawnY) or ""
     local shaman2Spawn = self.shaman2SpawnX and string.format([[<DC2 X="%s" Y="%s"/>]], self.shaman2SpawnX, self.shaman2SpawnY) or ""
 
-    return string.format([[<C><P/><Z><S>%s</S><D><DS X="%s" Y="%s"/>%s%s%s%s</D><O></O></Z></C>]], floors, self.playerSpawnX, self.playerSpawnY, shamanSpawn, shaman2Spawn, holes, cheeses)
+    return string.format([[<C><P L="%s" H="%s"/><Z><S>%s</S><D><DS X="%s" Y="%s"/>%s%s%s%s%s</D><O></O></Z></C>]], self.mapParameters.width, self.mapParameters.height, floors, self.playerSpawnX, self.playerSpawnY, shamanSpawn, shaman2Spawn, holes, cheeses, decorations)
 end
