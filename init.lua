c6811a2638da1d497e0c99a502bb96eb38fd2154
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-10-26 23:38:35 -0300

    change map background, add minPlayers variable

--- init.lua
+++ init.lua
@@ -2,7 +2,9 @@ for i, v in next, {"AutoNewGame", "AutoShaman", "AfkDeath", "PhysicalConsumables
     tfm.exec["disable" .. v]()
 end
 
-tfm.exec.newGame "<C><P MEDATA=';0,1;;;-0;0:::1-'/><Z><S><S T='6' X='400' Y='377' L='800' H='41' P='0,0,0.3,0.2,0,0,0,0'/></S><D><DS X='400' Y='342'/></D><O/><L/></Z></C>"
+local lobbyMap = "<C><P F='0' MEDATA=';0,1;;;-0;0:::1-'/><Z><S><S T='6' X='400' Y='377' L='800' H='41' P='0,0,0.3,0.2,0,0,0,0'/></S><D><DS X='400' Y='342'/></D><O/><L/></Z></C>"
+
+tfm.exec.newGame(lobbyMap)
 
 ui.setMapName("#dungeon")
 
@@ -16,4 +18,5 @@ local gameStates = {
     dungeon = 2,
 }
 
+local minPlayers = 1
 local currentGameState = 1
