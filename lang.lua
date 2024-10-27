d20a86556bd314739a7867e8e5937af4ad73804c
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-10-26 23:31:48 -0300

    add new some texts

--- lang.lua
+++ lang.lua
@@ -2,11 +2,13 @@ local texts = {
 
     br = {
 
-        help = "Ajuda"
+        help = "Ajuda",
+        helpText = "\tCada jogador nasce em uma parte da masmorra onde deve chegar ao centro da masmora enfrentando vários inimigos durante ao caminho, ao chegar ao final o jogador deve luta contra os outros jogadores que conseguiram chegar ao final e vencer"
     },
 
     en = {
 
-        help = "Help"
+        help = "Help",
+        helpText = "\tEach player is born in a part of the dungeon where they must reach the center of the dungeon, facing various enemies along the way, when reaching the end the player must fight against the other players who managed to reach the end and win."
     }
 }
