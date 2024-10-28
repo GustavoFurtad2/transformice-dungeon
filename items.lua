dbdca36883fb89ffd12deb79f290cfdc7dfc905b
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-10-28 18:00:34 -0300

    add Excalibur

--- items.lua
+++ items.lua
@@ -1,9 +1,68 @@
+local itemType = {
+
+    weapon = 1,
+    consumable = 2
+}
+
 local items = {
 
     sword = {
 
-        name = "Sword",
         index = 1,
 
+        name = "Sword",
+        type = itemType.weapon,
+
+        skills = {
+
+            [1] = {
+
+                name = "Quick Cut",
+
+                key = "X",
+                damage = 3,
+                cooldownTime = 2,
+
+            },
+
+            [2] = {
+
+                name = "Penetrate",
+
+                key = "Z",
+                damage = 7,
+                cooldownTime = 6,
+            }
+        },
+    },
+
+    excalibur = {
+
+        index = 2,
+
+        name = "Excalibur",
+        type = itemType.weapon,
+
+        skills = {
+
+            [1] = {
+
+                name = "Light Speed Cut",
+
+                key = "X",
+                damage = 7,
+                cooldownTime = 3,
+
+            },
+
+            [2] = {
+
+                name = "Sunrise",
+
+                key = "Z",
+                damage = 14,
+                cooldownTime = 7,
+            }
+        },
     }
 }
