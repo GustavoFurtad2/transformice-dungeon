bee9870eb0543bae4f02cab2a800e47aebd78df8
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-10-27 21:18:23 -0300

    reduced players limit

--- lobby.lua
+++ lobby.lua
@@ -9,8 +9,8 @@ local function showPlayerList(name)
 
     ui.addTextArea(-1, "<p align='center'><font size='40'>" .. startTimer.. "</font></p>", name, 0, 150, 800, 50, nil, nil, 2, true)
 
-    for i = 1, 10 do
-        ui.addTextArea(i, players[i] and players[i] or string.format("<a href='event:play_%s'>enter</a>", i), name, 0 + (i - 1) * 80, 300, 60, 25, nil, nil, 0.5, true)
+    for i = 1, 6 do
+        ui.addTextArea(i, players[i] and players[i] or string.format("<a href='event:play_%s'>enter</a>", i), name, 160 + (i - 1) * 80, 300, 60, 25, nil, nil, 0.5, true)
     end
 
 end
