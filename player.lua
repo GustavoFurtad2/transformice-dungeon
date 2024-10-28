eedd25217b4d7309739ba0ad5e567e63c9988646
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-10-27 21:13:42 -0300

    add hotbar

--- player.lua
+++ player.lua
@@ -16,18 +16,24 @@ function Player:new(name)
             isDead = false,
             isPlaying = false,
 
+            currentHotbar = 1,
+
             hotbar = {},
             backpack = {items.sword},
         },
     Player)
 
     player.hotbar[1] = player.backpack[1]
+    player.hotbar[2] = player.backpack[1]
 
     if currentGameState == gameStates.lobby then
 
         tfm.exec.respawnPlayer(name)
         showPlayerList(name)
     end
+
+    tfm.exec.bindKeyboard(name, string.byte("1"), true, true)
+    tfm.exec.bindKeyboard(name, string.byte("2"), true, true)
 
     return player
 end
@@ -64,19 +70,48 @@ end
 
 function Player:showHotbar(name)
 
-    ui.addTextArea(11, self.hotbar[1].name, name, 10, 345, 50, 50, nil, 0xf, 0.5, true)
+    ui.addTextArea(11, "<A:ACTIVE>1</A:ACTIVE>", name, 10, 345, 50, 50, nil, 0xf, 0.5, true)
         
     if self.hotbar[2] then
-        ui.addTextArea(12, self.hotbar[2].name, name, 75, 345, 50, 50, nil, 0xf, 0.5, true)
+        ui.addTextArea(12, "2", name, 75, 345, 50, 50, nil, 0xf, 0.5, true)
     end
         
     if self.hotbar[3] then
-        ui.addTextArea(13, self.hotbar[3].name, name, 140, 345, 50, 50, nil, 0xf, 0.5, true)
+        ui.addTextArea(13, "3", name, 140, 345, 50, 50, nil, 0xf, 0.5, true)
     end
 
     ui.addTextArea(14, string.format("<a href='event:help'>%s</a>", self.langPath.help), name, 760, 375, 35, 20, nil, 0xf, 0.5, true)
 end
 
+function Player:updateHotbar(name, hotbarNumber)
+
+    ui.updateTextArea(10 + self.currentHotbar, self.currentHotbar, name)
+    ui.updateTextArea(10 + hotbarNumber, "<A:ACTIVE>" .. hotbarNumber .. "</A:ACTIVE>", name)
+
+    self.currentHotbar = hotbarNumber
+end
+
 function Player:keyboard(key, down, x, y)
 
+    if key == string.byte("1") then
+        
+        if "1" ~= self.currentHotbar then
+
+            self:updateHotbar(name, 1)
+        end
+
+    elseif key == string.byte("2") then
+
+        if "2" ~= self.currentHotbar then
+
+            self:updateHotbar(name, 2)
+        end
+
+    elseif key == string.byte("3") then
+
+        if "3" ~= self.currentHotbar then
+
+            self:updateHotbar(name, 3)
+        end
+    end
 end
