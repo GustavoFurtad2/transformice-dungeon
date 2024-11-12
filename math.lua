9baaee29a0b926212ef0562b04d68d088dc7ac8d
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-11-11 23:33:50 -0300

    add checkPlayerNearby function

--- math.lua
+++ math.lua
@@ -7,3 +7,19 @@ local function checkDist(dist, player1, player2)
 
     return pythag(tfm.get.room.playerList[player1].x, tfm.get.room.playerList[player1].y, tfm.get.room.playerList[player2].x, tfm.get.room.playerList[player2].y) <= dist
 end
+
+local function checkPlayerNearby(dist, player1)
+
+    for name, object in next, data do
+
+        if name ~= player1 then
+
+            if checkDist(dist, player1, name) then
+
+                return name
+            end
+        end
+    end
+
+    return false
+end
