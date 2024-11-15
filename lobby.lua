fa7ea6e02c70bda6b458ece90d98a5d0837f1f22
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-11-14 22:47:26 -0300

    change maxPlayers to 6

--- lobby.lua
+++ lobby.lua
@@ -5,7 +5,7 @@ local startTimer = 10
 
 local function showPlayerList(name)
     
-    ui.addTextArea(0, string.format("<p align='center'><font size='40'>%s / %s players</font></p>", numberOfPlayers, 10), name, 0, 50, 800, 50, nil, nil, 0.5, true)
+    ui.addTextArea(0, string.format("<p align='center'><font size='40'>%s / %s players</font></p>", numberOfPlayers, 6), name, 0, 50, 800, 50, nil, nil, 0.5, true)
 
     ui.addTextArea(-1, "<p align='center'><font size='40'>" .. startTimer.. "</font></p>", name, 0, 150, 800, 50, nil, nil, 2, true)
 
